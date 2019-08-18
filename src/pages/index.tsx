90490ac2226f053aa2f271e889d0d517a156c0db
Michal Mackowiak <chemikpil@gmail.com>
2019-08-18 21:44:33 +0200

    📦Render Layout from gatsby browser plugin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 
-import Layout from '../components/Layout';
 import SEO from '../components/SEO';
 import { Content, Title, Summary } from '../components/UI/main';
 
 const IndexPage = () => (
-    <Layout>
+    <>
 		<SEO title='Personal site' />
 		<Content>
         	<Title>Michał Maćkowiak</Title>
@@ -16,7 +15,7 @@ const IndexPage = () => (
 				<span>Runner</span>
 			</Summary>
 		</Content>
-    </Layout>
+    </>
 );
 
 export default IndexPage;
