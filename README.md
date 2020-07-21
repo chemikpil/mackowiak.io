5f4d90c4efa2ea19787872cc794e501e4e0a7433
Michal Mackowiak <chemikpil@gmail.com>
2020-07-21 18:55:57 +0200

    👨‍💻 Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# mackowiak.io
+
+My personal place in the internet
+
+## 💻 Stack
+
+    * Next.js
+    * React.js
+    * Typescript
+    * Emotion.js
+    * TBD...
+
+---
+
+👨‍💻Made be [@chemikpil](https://twitter.com/chemikpil)
---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Mackowiak.io</title>
+      </Head>
+
+      <main>
+        <h1>Michał Maćkowiak</h1>
+        <p>TBD ...</p>
+      </main>
+    </div>
+  );
+}
