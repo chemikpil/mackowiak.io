e04b5da759efd395e55b3b917a63b14b67175255
Michał Maćkowiak <chemikpil@gmail.com>
2023-04-10 07:50:09 +0200

    🪽 Init tailwind.css and setup custom font

--- app/root.tsx
+++ app/root.tsx
@@ -6,13 +6,15 @@ import {
     Scripts,
     ScrollRestoration,
 } from '@remix-run/react';
-import {
+import type {
     DataFunctionArgs,
     LinksFunction,
     V2_MetaFunction,
 } from '@remix-run/node';
 import { getSeoMeta } from '~/utils/seo';
 import { getDomainUrl, getUrl } from '~/utils/misc';
+
+import stylesheet from './tailwind.css';
 
 export const meta: V2_MetaFunction = ({ data }) => {
     const url = getUrl(data.requestInfo);
@@ -29,26 +31,41 @@ export const meta: V2_MetaFunction = ({ data }) => {
 
 export const links: LinksFunction = () => [
     {
+        rel: 'preload',
+        as: 'font',
+        href: '/fonts/strawford-regular.woff2',
+        type: 'font/woff2',
+        crossOrigin: 'anonymous',
+    },
+    {
+        rel: 'preload',
+        as: 'font',
+        href: '/fonts/strawford-regularitalic.woff2',
+        type: 'font/woff2',
+        crossOrigin: 'anonymous',
+    },
+    {
         rel: 'icon',
         href: '/favicon.ico',
     },
     {
         rel: 'icon',
         type: 'image/png',
-        href: '/img/favicon-16x16.png',
+        href: '/images/favicon-16x16.png',
         sizes: '16x16',
     },
     {
         rel: 'icon',
         type: 'image/png',
-        href: '/img/favicon-32.png',
+        href: '/images/favicon-32.png',
         sizes: '32x32',
     },
     {
         rel: 'apple-touch-icon',
         sizes: '180x180',
-        href: '/img/apple-touch-icon.png',
+        href: '/images/apple-touch-icon.png',
     },
+    { rel: 'stylesheet', href: stylesheet },
 ];
 
 export async function loader({ request }: DataFunctionArgs) {
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,3 +1,3 @@
 export default function Index() {
-    return <div>Home page</div>;
+    return <div className="text">Home page</div>;
 }
