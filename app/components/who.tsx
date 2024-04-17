e180ed05900a6c3fbd47b7135ea899f50577a31d
Michał Maćkowiak <chemikpil@gmail.com>
2024-04-17 19:39:04 +0200

    👨🏻‍💻 Final version of animated Who I'm section

--- app/components/who.tsx
+++ app/components/who.tsx
@@ -1,5 +1,13 @@
 import { useEffect, useState } from 'react'
-import { AnimatePresence, motion } from 'framer-motion'
+import {
+	AnimatePresence,
+	motion,
+	MotionValue,
+	useSpring,
+	useTransform,
+} from 'framer-motion'
+
+import { useMediaQuery, breakpoints } from '~/hooks/useMediaQuery'
 
 const items = [
 	'👨🏻‍💻 Frontend Engineer',
@@ -9,37 +17,68 @@ const items = [
 	'⚽️ Football Fan',
 ]
 
-const animationConfig = {
-	initial: { opacity: 0, y: 50 },
-	enter: { opacity: 1, y: 0, transition: { duration: 0.3 } },
-	exit: { opacity: 0, y: -20, transition: { duration: 0.3 } },
+function Item({
+	mv,
+	index,
+	height,
+}: {
+	mv: MotionValue
+	index: number
+	height: number
+}) {
+	let y = useTransform(mv, latest => {
+		const length = items.length
+		const placedValue = latest % length
+		const offset = (length + index - placedValue) % length
+		let memo = height * offset
+
+		if (offset > 2) {
+			memo -= 5 * height
+		}
+
+		return memo
+	})
+
+	return (
+		<motion.p
+			style={{ y }}
+			className="absolute p-2 text-xl md:text-2xl lg:text-4xl"
+		>
+			{items[index]}
+		</motion.p>
+	)
 }
 
 export function Who() {
 	const [current, setCurrent] = useState(0)
-	const { initial, enter, exit } = animationConfig
+	const [height, setHeight] = useState(0)
+	const animatedValue = useSpring(current, { bounce: 0 })
+	const lg = useMediaQuery(breakpoints.lg)
+	const md = useMediaQuery(breakpoints.md)
+
+	useEffect(() => {
+		setHeight((lg ? 40 : md ? 32 : 28) + 16)
+	}, [])
+
+	useEffect(() => {
+		animatedValue.set(current)
+	}, [current])
 
 	useEffect(() => {
 		const interval = setInterval(() => {
-			setCurrent(current => (current + 1) % items.length)
-		}, 3000)
+			setCurrent(current => current + 1)
+		}, 2000)
 
 		return () => clearInterval(interval)
 	}, [])
 
 	return (
-		<div className="flex w-full justify-center">
-			<AnimatePresence initial={false} mode="popLayout">
-				<motion.p
-					initial={initial}
-					animate={enter}
-					exit={exit}
-					className="whitespace-nowrap text-xl md:text-2xl lg:text-4xl"
-					key={current}
-				>
-					{items[current]}
-				</motion.p>
-			</AnimatePresence>
+		<div className="w-full overflow-hidden">
+			<div style={{ height }} className="relative flex w-full justify-center">
+				{items.map((_, index) => (
+					<Item mv={animatedValue} index={index} key={index} height={height} />
+				))}
+			</div>
 		</div>
 	)
 }
