34fa8ec91c587f20fdf7f5a20ff7717370747d84
Michał Maćkowiak <chemikpil@gmail.com>
2024-04-29 13:25:01 +0200

    🤯 Fix header background

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -2,7 +2,7 @@ import { Logo } from '~/components/logo'
 
 export function Header() {
 	return (
-		<header className="sticky top-0 flex place-items-center justify-center py-6 px-10">
+		<header className="from-background via-background sticky top-0 z-10 flex place-items-center justify-center bg-gradient-to-b py-6 px-10 pb-20">
 			<Logo className="fill-foreground h-12 w-12" />
 		</header>
 	)
